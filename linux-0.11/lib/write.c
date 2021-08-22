1dca95b434ffc9f0fed8f2c2e7889658bf690203
Steven Kang <kangweibaby@163.com>
2021-08-23 01:28:33 +0800

    🎨 注释 lib/write.c

--- linux-0.11/lib/write.c
+++ linux-0.11/lib/write.c
@@ -7,4 +7,14 @@
 #define __LIBRARY__
 #include <unistd.h>
 
-_syscall3(int,write,int,fd,const char *,buf,off_t,count)
+// 写文件系统调用函数
+// 该宏结构对应于函数：
+// int write(int fd, const char * buf, off_t count)
+// 参数：
+// fd - 文件描述符；
+// buf - 写缓冲区指针；
+// count - 写字节数
+// 返回：
+// 成功时返回写入的字节数(0 表示写入0 字节)；
+// 出错时将返回-1，并且设置了出错号
+_syscall3(int, write, int, fd, const char *, buf, off_t, count)
