9d89564f4409154babde1367543ba720c960b2aa
Steven Kang <kangweibaby@163.com>
2021-08-23 01:16:18 +0800

    🎨 注释 lib/close.c

--- linux-0.11/lib/close.c
+++ linux-0.11/lib/close.c
@@ -7,4 +7,8 @@
 #define __LIBRARY__
 #include <unistd.h>
 
-_syscall1(int,close,int,fd)
+// 关闭文件函数。
+// 下面该调用宏函数对应：int close(int fd)
+// 直接调用了系统中断int 0x80，参数是 __NR_close
+// 其中 fd 是文件描述符
+_syscall1(int, close, int, fd)
