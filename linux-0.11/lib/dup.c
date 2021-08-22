ec5065db38d12ec2794125b699fc46798b50e012
Steven Kang <kangweibaby@163.com>
2021-08-23 01:18:56 +0800

    🎨 注释 lib/dup.c

--- linux-0.11/lib/dup.c
+++ linux-0.11/lib/dup.c
@@ -7,4 +7,8 @@
 #define __LIBRARY__
 #include <unistd.h>
 
-_syscall1(int,dup,int,fd)
+// 复制文件描述符函数
+// 下面该调用宏函数对应：int dup(int fd)
+// 直接调用了系统中断 int 0x80，参数是 __NR_dup
+// 其中 fd 是文件描述符
+_syscall1(int, dup, int, fd)
