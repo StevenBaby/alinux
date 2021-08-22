ca221b4fb3f59377b9a01cb2c46369d5daca3ff6
Steven Kang <kangweibaby@163.com>
2021-08-23 01:15:11 +0800

    🎨 注释 lib/_exit.c

--- linux-0.11/lib/_exit.c
+++ linux-0.11/lib/_exit.c
@@ -5,9 +5,15 @@
  */
 
 #define __LIBRARY__
+
+// Linux 标准头文件，定义了各种符号常数和类型，并申明了各种函数
+// 如定义了 __LIBRARY__，则还含系统调用号和内嵌汇编 syscall0() 等
 #include <unistd.h>
 
+// 内核使用的程序(退出)终止函数
+// 直接调用系统中断 int 0x80，功能号 __NR_exit
+// 参数：exit_code - 退出码。
 void _exit(int exit_code)
 {
-	__asm__ __volatile__ ("int $0x80"::"a" (__NR_exit),"b" (exit_code));
+	__asm__ __volatile__("int $0x80" ::"a"(__NR_exit), "b"(exit_code));
 }
