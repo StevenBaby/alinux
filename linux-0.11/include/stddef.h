2eac37f5fae197462eed185e0f1286fed1f88473
Steven Kang <kangweibaby@163.com>
2021-08-27 19:50:48 +0800

    🎨 注释 include/stddef.h

--- linux-0.11/include/stddef.h
+++ linux-0.11/include/stddef.h
@@ -3,17 +3,18 @@
 
 #ifndef _PTRDIFF_T
 #define _PTRDIFF_T
-typedef long ptrdiff_t;
+typedef long ptrdiff_t; // 两个指针相减结果的类型
 #endif
 
 #ifndef _SIZE_T
 #define _SIZE_T
-typedef unsigned long size_t;
+typedef unsigned long size_t; // sizeof 返回的类型
 #endif
 
 #undef NULL
-#define NULL ((void *)0)
+#define NULL ((void *)0) // 空指针
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+// 成员在类型中的偏移位置
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
 
 #endif
