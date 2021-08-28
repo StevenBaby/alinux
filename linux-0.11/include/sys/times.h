eedc105102b7cc62a1676213690f6a6648c7772f
Steven Kang <kangweibaby@163.com>
2021-08-28 22:44:14 +0800

    🎨 注释 include/sys/times.h

--- linux-0.11/include/sys/times.h
+++ linux-0.11/include/sys/times.h
@@ -3,13 +3,14 @@
 
 #include <sys/types.h>
 
-struct tms {
-	time_t tms_utime;
-	time_t tms_stime;
-	time_t tms_cutime;
-	time_t tms_cstime;
+struct tms
+{
+	time_t tms_utime;  // 用户使用的 CPU 时间
+	time_t tms_stime;  // 系统（内核）CPU 时间
+	time_t tms_cutime; // 已终止的子进程使用的用户 CPU 时间
+	time_t tms_cstime; // 已终止的子进程使用的系统 CPU 时间
 };
 
-extern time_t times(struct tms * tp);
+extern time_t times(struct tms *tp);
 
 #endif
