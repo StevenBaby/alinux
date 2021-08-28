1b1e6169472f1b682ef2fd52e1acac0ef8efe387
Steven Kang <kangweibaby@163.com>
2021-08-28 22:42:18 +0800

    🎨 注释 include/sys/stat.h

--- linux-0.11/include/sys/stat.h
+++ linux-0.11/include/sys/stat.h
@@ -3,56 +3,76 @@
 
 #include <sys/types.h>
 
-struct stat {
-	dev_t	st_dev;
-	ino_t	st_ino;
-	umode_t	st_mode;
-	nlink_t	st_nlink;
-	uid_t	st_uid;
-	gid_t	st_gid;
-	dev_t	st_rdev;
-	off_t	st_size;
-	time_t	st_atime;
-	time_t	st_mtime;
-	time_t	st_ctime;
+struct stat
+{
+	dev_t st_dev;	  // 含有文件的设备号
+	ino_t st_ino;	  // 文件i 节点号
+	umode_t st_mode;  // 文件类型和属性
+	nlink_t st_nlink; // 指定文件的连接数
+	uid_t st_uid;	  // 文件的用户(标识)号
+	gid_t st_gid;	  // 文件的组号
+	dev_t st_rdev;	  // 设备号(如果文件是特殊的字符文件或块文件)
+	off_t st_size;	  // 文件大小（字节数）（如果文件是常规文件）
+	time_t st_atime;  // 上次（最后）访问时间
+	time_t st_mtime;  // 最后修改时间
+	time_t st_ctime;  // 最后节点修改时间
 };
 
-#define S_IFMT  00170000
-#define S_IFREG  0100000
-#define S_IFBLK  0060000
-#define S_IFDIR  0040000
-#define S_IFCHR  0020000
-#define S_IFIFO  0010000
-#define S_ISUID  0004000
-#define S_ISGID  0002000
-#define S_ISVTX  0001000
+// st_mode 值的符号名称
+// 文件类型
+#define S_IFMT 00170000 // 文件类型（8 进制表示）
+#define S_IFREG 0100000 // 常规文件
+#define S_IFBLK 0060000 // 块特殊（设备）文件，如磁盘 dev/fd0
+#define S_IFDIR 0040000 // 目录文件
+#define S_IFCHR 0020000 // 字符设备文件
+#define S_IFIFO 0010000 // FIFO 特殊文件
 
-#define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)
-#define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
-#define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
-#define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
-#define S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)
+// 文件属性位：
+// S_ISUID 用于测试文件的 set-user-ID 标志是否置位
+// 若该标志置位，则当执行该文件时，进程的有效用户ID
+// 将被设置为该文件宿主的用户 ID，S_ISGID 则是针对组 ID 进行相同处理
+#define S_ISUID 0004000 // 执行时设置用户ID（set-user-ID）
+#define S_ISGID 0002000 // 执行时设置组ID（set-group-ID）
+#define S_ISVTX 0001000 // 对于目录，受限删除标志
 
-#define S_IRWXU 00700
-#define S_IRUSR 00400
-#define S_IWUSR 00200
-#define S_IXUSR 00100
+#define S_ISREG(m) (((m)&S_IFMT) == S_IFREG)  // 测试是否常规文件
+#define S_ISDIR(m) (((m)&S_IFMT) == S_IFDIR)  // 是否目录文件
+#define S_ISCHR(m) (((m)&S_IFMT) == S_IFCHR)  // 是否字符设备文件
+#define S_ISBLK(m) (((m)&S_IFMT) == S_IFBLK)  // 是否块设备文件
+#define S_ISFIFO(m) (((m)&S_IFMT) == S_IFIFO) // 是否FIFO 特殊文件
 
-#define S_IRWXG 00070
-#define S_IRGRP 00040
-#define S_IWGRP 00020
-#define S_IXGRP 00010
+// 文件访问权限
+#define S_IRWXU 00700 // 宿主可以读、写、执行/搜索
+#define S_IRUSR 00400 // 宿主读许可
+#define S_IWUSR 00200 // 宿主写许可
+#define S_IXUSR 00100 // 宿主执行/搜索许可
 
-#define S_IRWXO 00007
-#define S_IROTH 00004
-#define S_IWOTH 00002
-#define S_IXOTH 00001
+#define S_IRWXG 00070 // 组成员可以读、写、执行/搜索
+#define S_IRGRP 00040 // 组成员读许可
+#define S_IWGRP 00020 // 组成员写许可
+#define S_IXGRP 00010 // 组成员执行/搜索许可
 
+#define S_IRWXO 00007 // 其他人读、写、执行/搜索许可
+#define S_IROTH 00004 // 其他人读许可
+#define S_IWOTH 00002 // 其他人写许可
+#define S_IXOTH 00001 // 其他人执行/搜索许可
+
+// 修改文件属性
 extern int chmod(const char *_path, mode_t mode);
+
+// 取指定文件句柄的文件状态信息
 extern int fstat(int fildes, struct stat *stat_buf);
+
+// 创建目录
 extern int mkdir(const char *_path, mode_t mode);
+
+// 创建管道文件
 extern int mkfifo(const char *_path, mode_t mode);
+
+// 取指定文件名的文件状态信息
 extern int stat(const char *filename, struct stat *stat_buf);
+
+// 设置属性屏蔽码
 extern mode_t umask(mode_t mask);
 
 #endif
