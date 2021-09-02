775bdffa9d87fe578e70207cc9f7db164f010c6d
Steven Kang <kangweibaby@163.com>
2021-09-02 22:48:12 +0800

    🎨 注释 fs/char_dev.c

--- linux-0.11/fs/char_dev.c
+++ linux-0.11/fs/char_dev.c
@@ -13,92 +13,133 @@
 #include <asm/segment.h>
 #include <asm/io.h>
 
-extern int tty_read(unsigned minor,char * buf,int count);
-extern int tty_write(unsigned minor,char * buf,int count);
+// 终端读
+extern int tty_read(unsigned minor, char *buf, int count);
 
-typedef int (*crw_ptr)(int rw,unsigned minor,char * buf,int count,off_t * pos);
+// 终端写
+extern int tty_write(unsigned minor, char *buf, int count);
 
-static int rw_ttyx(int rw,unsigned minor,char * buf,int count,off_t * pos)
+// 定义字符设备读写函数指针类型
+typedef int (*crw_ptr)(int rw, unsigned minor, char *buf, int count, off_t *pos);
+
+// 串口终端读写操作函数
+// 参数：rw - 读写命令；minor - 终端子设备号；buf - 缓冲区；cout - 读写字节数；
+// pos - 读写操作当前指针，对于终端操作，该指针无用
+// 返回：实际读写的字节数。
+static int rw_ttyx(int rw, unsigned minor, char *buf, int count, off_t *pos)
 {
-	return ((rw==READ)?tty_read(minor,buf,count):
-		tty_write(minor,buf,count));
+	return ((rw == READ) ? tty_read(minor, buf, count) : tty_write(minor, buf, count));
 }
 
-static int rw_tty(int rw,unsigned minor,char * buf,int count, off_t * pos)
+// 终端读写操作函数
+// 同上rw_ttyx()，只是增加了对进程是否有控制终端的检测
+static int rw_tty(int rw, unsigned minor, char *buf, int count, off_t *pos)
 {
-	if (current->tty<0)
+	// 若进程没有对应的控制终端，则返回出错号
+	if (current->tty < 0)
 		return -EPERM;
-	return rw_ttyx(rw,current->tty,buf,count,pos);
+
+	// 否则调用终端读写函数 rw_ttyx()，并返回实际读写字节数
+	return rw_ttyx(rw, current->tty, buf, count, pos);
 }
 
-static int rw_ram(int rw,char * buf, int count, off_t *pos)
+// 内存数据读写，未实现
+static int rw_ram(int rw, char *buf, int count, off_t *pos)
 {
 	return -EIO;
 }
 
-static int rw_mem(int rw,char * buf, int count, off_t * pos)
+// 内存数据读写操作函数，未实现
+static int rw_mem(int rw, char *buf, int count, off_t *pos)
 {
 	return -EIO;
 }
 
-static int rw_kmem(int rw,char * buf, int count, off_t * pos)
+// 内核数据区读写函数，未实现
+static int rw_kmem(int rw, char *buf, int count, off_t *pos)
 {
 	return -EIO;
 }
 
-static int rw_port(int rw,char * buf, int count, off_t * pos)
+// 端口读写操作函数
+// 参数：rw - 读写命令；buf - 缓冲区；cout - 读写字节数；pos - 端口地址；
+// 返回：实际读写的字节数。
+static int rw_port(int rw, char *buf, int count, off_t *pos)
 {
-	int i=*pos;
+	int i = *pos;
 
-	while (count-->0 && i<65536) {
-		if (rw==READ)
-			put_fs_byte(inb(i),buf++);
+	// 对于所要求读写的字节数，并且端口地址小于 64k 时，循环执行单个字节的读写操作
+	while (count-- > 0 && i < 65536)
+	{
+		// 若是读命令，则从端口 i 中读取一字节内容并放到用户缓冲区中
+		if (rw == READ)
+			put_fs_byte(inb(i), buf++);
+		// 若是写命令，则从用户数据缓冲区中取一字节输出到端口 i
 		else
-			outb(get_fs_byte(buf++),i);
+			outb(get_fs_byte(buf++), i);
+
+		// 前移一个端口 [??]
 		i++;
 	}
+
+	// 计算读/写的字节数，并相应调整读写指针
 	i -= *pos;
 	*pos += i;
+
+	// 返回读/写的字节数
 	return i;
 }
 
-static int rw_memory(int rw, unsigned minor, char * buf, int count, off_t * pos)
+// 内存读写操作函数
+static int rw_memory(int rw, unsigned minor, char *buf, int count, off_t *pos)
 {
-	switch(minor) {
-		case 0:
-			return rw_ram(rw,buf,count,pos);
-		case 1:
-			return rw_mem(rw,buf,count,pos);
-		case 2:
-			return rw_kmem(rw,buf,count,pos);
-		case 3:
-			return (rw==READ)?0:count;	/* rw_null */
-		case 4:
-			return rw_port(rw,buf,count,pos);
-		default:
-			return -EIO;
+	// 根据内存设备子设备号，分别调用不同的内存读写函数
+	switch (minor)
+	{
+	case 0:
+		return rw_ram(rw, buf, count, pos);
+	case 1:
+		return rw_mem(rw, buf, count, pos);
+	case 2:
+		return rw_kmem(rw, buf, count, pos);
+	case 3:
+		return (rw == READ) ? 0 : count; /* rw_null */
+	case 4:
+		return rw_port(rw, buf, count, pos);
+	default:
+		return -EIO;
 	}
 }
 
-#define NRDEVS ((sizeof (crw_table))/(sizeof (crw_ptr)))
+// 定义系统中设备种数
+#define NRDEVS ((sizeof(crw_table)) / (sizeof(crw_ptr)))
 
-static crw_ptr crw_table[]={
-	NULL,		/* nodev */
-	rw_memory,	/* /dev/mem etc */
-	NULL,		/* /dev/fd */
-	NULL,		/* /dev/hd */
-	rw_ttyx,	/* /dev/ttyx */
-	rw_tty,		/* /dev/tty */
-	NULL,		/* /dev/lp */
-	NULL};		/* unnamed pipes */
+// 字符设备读写函数指针表
+static crw_ptr crw_table[] = {
+	NULL,	   // 无设备(空设备)
+	rw_memory, // /dev/mem 等
+	NULL,	   // /dev/fd 软驱
+	NULL,	   // /dev/hd 硬盘
+	rw_ttyx,   // /dev/ttyx 串口终端
+	rw_tty,	   // /dev/tty 终端
+	NULL,	   // /dev/lp 打印机
+	NULL};	   // 未命名管道
 
-int rw_char(int rw,int dev, char * buf, int count, off_t * pos)
+// 字符设备读写操作函数
+// 参数：rw - 读写命令；dev - 设备号；buf - 缓冲区；count - 读写字节数；pos -读写指针；
+// 返回：实际读/写字节数
+int rw_char(int rw, int dev, char *buf, int count, off_t *pos)
 {
 	crw_ptr call_addr;
 
-	if (MAJOR(dev)>=NRDEVS)
+	// 如果设备号超出系统设备数，则返回出错码
+	if (MAJOR(dev) >= NRDEVS)
 		return -ENODEV;
-	if (!(call_addr=crw_table[MAJOR(dev)]))
+
+	// 若该设备没有对应的读/写函数，则返回出错码
+	if (!(call_addr = crw_table[MAJOR(dev)]))
 		return -ENODEV;
-	return call_addr(rw,MINOR(dev),buf,count,pos);
+
+	// 调用对应设备的读写操作函数，并返回实际读/写的字节数
+	return call_addr(rw, MINOR(dev), buf, count, pos);
 }
